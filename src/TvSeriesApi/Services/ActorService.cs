56133ba6e267106324a6b658bf49237583921e73
Artgelika <artgelika.com>
2022-08-01 13:58:04 +0200

    Add actor interface

--- src/TvSeriesApi/Services/ActorService.cs
+++ src/TvSeriesApi/Services/ActorService.cs
@@ -1,6 +1,37 @@
-﻿namespace TvSeriesApi.Services
+﻿
+
+namespace TvSeriesApi.Services
 {
-    public class ActorService
+    public class ActorService : IActorService
     {
+        public Task<ActorReadDTO> AddActorAsync(ActorCreateDTO name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActorReadDTO> AddActorByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteActorAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditActorAsync(int id, ActorUpdateDTO actorDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActorReadDTO> GetActorByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ActorReadDTO>> GetAllActorsAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
