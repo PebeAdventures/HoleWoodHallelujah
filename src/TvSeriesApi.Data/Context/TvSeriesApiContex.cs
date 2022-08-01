039835c6c90ba7ae576de61cfe13dfd5654058d0
we3py <CRV374@motorolasolutions.com>
2022-08-01 11:56:20 +0200

    Create Context class

--- src/TvSeriesApi.Data/Context/TvSeriesApiContex.cs
+++ src/TvSeriesApi.Data/Context/TvSeriesApiContex.cs
@@ -1,6 +1,19 @@
-﻿namespace TvSeriesApi.Data.Context
+﻿using Microsoft.EntityFrameworkCore;
+using TvSeriesApi.Data.Entities;
+
+namespace TvSeriesApi.Data.Context
 {
-    public class TvSeriesApiContex
+    public class TvSeriesApiContex : DbContext
     {
+        public DbSet<TVSeries> TVSeries { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Season> Seasons { get; set; }
+        public DbSet<Episode> Episodes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
