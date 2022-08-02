3b4b51ea9d91693874df466836111fc6ae33df17
we3py <CRV374@motorolasolutions.com>
2022-08-02 14:12:24 +0200

    Fullfill EpisodeController

---
+++ src/TvSeriesApi/Controllers/EpisodesControllers.cs
@@ -0,0 +1,47 @@
+﻿namespace TvSeriesApi.Controllers
+{
+    [Route("api/Episodes")]
+    [ApiController]
+    public class EpisodesControllers : ControllerBase
+    {
+        private IEpisodeService _episodeService;
+
+        public EpisodesControllers(IEpisodeService episodeService)
+        {
+            _episodeService = episodeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEpisode(int id)
+        {
+            var episode = await _episodeService.GetEpisodeByIdAsync(id);
+            if (episode == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(episode);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteEpisode(int id)
+        {
+            await _episodeService.DeleteEpisodeAsync(id);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEpisode(EpisodeCreateDTO episode)
+        {
+            await _episodeService.CreateEpisode(episode);
+            return Created("", episode);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEpisode(EpisodeUpdateDTO episode)
+        {
+            await _episodeService.UpdateEpisode(episode);
+            return Created("", episode);
+        }
+    }
+}
