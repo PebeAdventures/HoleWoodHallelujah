c821f4886125c3fd312f0e841441ffd5c1f02c43
we3py <CRV374@motorolasolutions.com>
2022-08-02 11:45:40 +0200

    Create default maps for entitis and DTO objects

---
+++ src/TvSeriesApi/MapperProfile.cs
@@ -0,0 +1,29 @@
+﻿namespace TvSeriesApi
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Actor, ActorReadDTO>();
+            CreateMap<Actor, ActorWithTvSeriesDTO>();
+            CreateMap<ActorCreateDTO, Actor>();
+            CreateMap<ActorUpdateDTO, Actor>();
+
+            CreateMap<Episode, EpisodeReadDTO>();
+            CreateMap<EpisodeCreateDTO, Episode>();
+            CreateMap<EpisodeUpdateDTO, Episode>();
+
+            CreateMap<Genre, GenreReadDTO>();
+            CreateMap<GenreCreateDTO, Genre>();
+            CreateMap<GenreUpdateDTO, Genre>();
+
+            CreateMap<Season, SeasonReadDTO>();
+            CreateMap<SeasonCreateDTO, Season>();
+            CreateMap<SeasonUpdateDTO, Season>();
+
+            CreateMap<TVSeries, TVSeriesReadDTO>();
+            CreateMap<TVSeriesCreateDTO, TVSeries>();
+            CreateMap<TVSeriesUpdateDTO, TVSeries>();
+        }
+    }
+}
