65fc546f78d419f439c1cfe924da0a67bab09fe9
we3py <CRV374@motorolasolutions.com>
2022-08-02 14:09:17 +0200

    Create class for specific mapping

---
+++ src/TvSeriesApi/Profiles/EpisodeSeasonNameResolver.cs
@@ -0,0 +1,10 @@
+﻿namespace TvSeriesApi
+{
+    public class EpisodeSeasonNameResolver : IValueResolver<Episode, EpisodeReadDTO, string>
+    {
+        public string Resolve(Episode source, EpisodeReadDTO destination, string destMember, ResolutionContext context)
+        {
+            return source.Season.Name;
+        }
+    }
+}
