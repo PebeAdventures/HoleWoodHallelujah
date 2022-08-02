5e7c2c933f0462a4ab159ad8f57a1f1b135e954d
we3py <CRV374@motorolasolutions.com>
2022-08-02 14:10:41 +0200

    Fullfill class with methods

--- src/TvSeriesApi/Services/EpisodeService.cs
+++ src/TvSeriesApi/Services/EpisodeService.cs
@@ -1,6 +1,38 @@
-﻿namespace TvSeriesApi.Services
+﻿
+
+namespace TvSeriesApi.Services
 {
-    public class EpisodeService
+    public class EpisodeService : IEpisodeService
     {
+        private IUnitOfWork _unitOfWork;
+        private IMapper _mapper;
+        public EpisodeService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public Task CreateEpisode(EpisodeCreateDTO episode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEpisodeAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<EpisodeReadDTO> GetEpisodeByIdAsync(int id)
+        {
+            var episode = await _unitOfWork.Episodes.GetEpisodeWithAlbum(id);
+            var episodeDTO = _mapper.Map<EpisodeReadDTO>(episode);
+
+            return episodeDTO;
+        }
+
+        public Task UpdateEpisode(EpisodeUpdateDTO episode)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
