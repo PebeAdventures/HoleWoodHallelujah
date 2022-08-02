7069a31a9a079d4b4d6ba8e738b2bda443b0e2f6
Artgelika <artgelika.com>
2022-08-02 14:50:41 +0200

    Add Actor contructor

--- src/TvSeriesApi.Data/Entities/Actor.cs
+++ src/TvSeriesApi.Data/Entities/Actor.cs
@@ -6,5 +6,10 @@
         public string Fullname { get; set; }
         public int Age { get; set; }
         public IEnumerable<TVSeries> TVSeries { get; set; } = new List<TVSeries>();
+
+        public Actor()
+        {
+            TVSeries = new List<TVSeries>();
+        }
     }
 }
