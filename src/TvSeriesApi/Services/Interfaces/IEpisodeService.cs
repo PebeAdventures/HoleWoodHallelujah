3e08b7104abd25e8aa66564311390279ed00d673
we3py <CRV374@motorolasolutions.com>
2022-08-03 09:48:44 +0200

    Change return object

--- src/TvSeriesApi/Services/Interfaces/IEpisodeService.cs
+++ src/TvSeriesApi/Services/Interfaces/IEpisodeService.cs
@@ -3,7 +3,7 @@
     public interface IEpisodeService
     {
         // TODO zastanowić się czy szukanie odcinków po serialu oraz szukanie po serialu i sezonie powinno być w IEpisode czy odpowiednio w ISeasons oraz ISeries
-        Task<EpisodeReadDTO> GetEpisodeByIdAsync(int id);
+        Task<OperationResult<EpisodeReadDTO>> GetEpisodeByIdAsync(int id);
         Task DeleteEpisodeAsync(int id);
         Task CreateEpisode(EpisodeCreateDTO episode);
         Task UpdateEpisodeAsync(EpisodeUpdateDTO episode);
