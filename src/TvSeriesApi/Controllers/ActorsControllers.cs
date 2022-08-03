a46c8bb22d7263d59021db6e8d7d17186ac4182d
Artgelika <artgelika.com>
2022-08-03 12:01:51 +0200

    Add GetById method to Actor service

--- src/TvSeriesApi/Controllers/ActorsControllers.cs
+++ src/TvSeriesApi/Controllers/ActorsControllers.cs
@@ -1,6 +1,6 @@
 ﻿namespace TvSeriesApi.Controllers
 {
-    [Route("api/actors")]
+    [Route("api/Actors")]
     [ApiController]
     public class ActorsControllers : ControllerBase
     {
@@ -23,62 +23,74 @@
             return Ok(actors);
         }
 
+        [SwaggerOperation(Summary = "Retrieves specific Artist")]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var artist = await _actorService.GetActorByIdAsync(id);
+            if (artist is not null)
+            {
+                return Ok(artist);
+            }
+            return NotFound();
+        }
 
-////        GET api/actors/{id}
-//    }
-//    [SwaggerOperation(Summary = "Get actor by id")]
-//    [HttpGet("{id}", Name = "GetActorByIdAsync")]
-//    public async Task<IActionResult> GetActorByIdAsync(int id)
-//    {
-//        var actor = await _actorService.GetActorByIdAsync(id);
-//        return Ok(actor);
-//    }
 
-//    GET api/actors/{name}
-//}
-//[SwaggerOperation(Summary = "Get actor by name")]
-//[HttpGet("{name}", Name = "GetActorByName")]
-//public async Task<IActionResult> GetActorByName(string name)
-//{
-//    var actor = await _actorService.GetActorByName(name);
-//    return Ok(actor);
-//}
+        ////        GET api/actors/{id}
+        //    }
+        //    [SwaggerOperation(Summary = "Get actor by id")]
+        //    [HttpGet("{id}", Name = "GetActorByIdAsync")]
+        //    public async Task<IActionResult> GetActorByIdAsync(int id)
+        //    {
+        //        var actor = await _actorService.GetActorByIdAsync(id);
+        //        return Ok(actor);
+        //    }
 
-//POST api/actors
-//        [SwaggerOperation(Summary = "Create actor by DTO data")]
-//[HttpPost]
-//public async Task<IActionResult> CreateActorAsync(ActorCreateDTO actorCreateDTO)
-//{
-//    var actorToCreate = await _actorService.AddActorAsync(actorCreateDTO);
-//    return Ok(actorToCreate);
-//}
+        //    GET api/actors/{name}
+        //}
+        //[SwaggerOperation(Summary = "Get actor by name")]
+        //[HttpGet("{name}", Name = "GetActorByName")]
+        //public async Task<IActionResult> GetActorByName(string name)
+        //{
+        //    var actor = await _actorService.GetActorByName(name);
+        //    return Ok(actor);
+        //}
 
-////POST api/actors
-//        [SwaggerOperation(Summary = "Create actor by name")]
-//[HttpPost]
-//public async Task<IActionResult> CreateActorByNameAsync(string actorToAdd)
-//{
-//    var actorToUpdate = await _actorService.AddActorByNameAsync(actorToAdd);
-//    return Ok(actorToUpdate);
-//}
+        //POST api/actors
+        //        [SwaggerOperation(Summary = "Create actor by DTO data")]
+        //[HttpPost]
+        //public async Task<IActionResult> CreateActorAsync(ActorCreateDTO actorCreateDTO)
+        //{
+        //    var actorToCreate = await _actorService.AddActorAsync(actorCreateDTO);
+        //    return Ok(actorToCreate);
+        //}
 
-////PUT api/actors/{id}
-//        [SwaggerOperation(Summary = "Update actor by id")]
-//[HttpPut("{id}")]
-//public async Task<IActionResult> UpdateActorById(int id, ActorUpdateDTO actorUpdateDTO)
-//{
-//    await _actorService.EditActorAsync(id, actorUpdateDTO);
-//    return NoContent();
-//}
+        ////POST api/actors
+        //        [SwaggerOperation(Summary = "Create actor by name")]
+        //[HttpPost]
+        //public async Task<IActionResult> CreateActorByNameAsync(string actorToAdd)
+        //{
+        //    var actorToUpdate = await _actorService.AddActorByNameAsync(actorToAdd);
+        //    return Ok(actorToUpdate);
+        //}
 
-////DELETE api/actors/{id}
-//        [SwaggerOperation(Summary = "Delete actor by id")]
-//[HttpDelete("{id}")]
-//public async Task<IActionResult> DeleteActorById(int id)
-//{
-//    await _actorService.DeleteActorAsync(id);
-//    return NoContent();
+        ////PUT api/actors/{id}
+        //        [SwaggerOperation(Summary = "Update actor by id")]
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> UpdateActorById(int id, ActorUpdateDTO actorUpdateDTO)
+        //{
+        //    await _actorService.EditActorAsync(id, actorUpdateDTO);
+        //    return NoContent();
+        //}
 
-//}
+        ////DELETE api/actors/{id}
+        //        [SwaggerOperation(Summary = "Delete actor by id")]
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteActorById(int id)
+        //{
+        //    await _actorService.DeleteActorAsync(id);
+        //    return NoContent();
+
+        //}
     }
 }
