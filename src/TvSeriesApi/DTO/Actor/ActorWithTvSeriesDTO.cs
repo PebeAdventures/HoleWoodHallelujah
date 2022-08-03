01597b2a78047c8f5b1691cfba6fcc7302b8ac33
Artgelika <artgelika.com>
2022-08-03 14:55:22 +0200

    Fix linst into List<string>

--- src/TvSeriesApi/DTO/Actor/ActorWithTvSeriesDTO.cs
+++ src/TvSeriesApi/DTO/Actor/ActorWithTvSeriesDTO.cs
@@ -5,6 +5,6 @@ namespace TvSeriesApi.DTO.Actor
     public class ActorWithTvSeriesDTO
     {
         public string Fullname { get; set; }
-        public IEnumerable<TVSeriesReadDTO> tVSeriesReadDTOs { get; set; }
+        public IEnumerable<string> TvSeriesName { get; set; }
     }
 }
