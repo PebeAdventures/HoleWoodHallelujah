5ff48f5bfc22b85cbfb6e1354c00bbd716d501c2
we3py <CRV374@motorolasolutions.com>
2022-08-03 11:55:06 +0200

    Create method to get all records

--- src/TvSeriesApi.Data/DAL/Interfaces/IEpisodeRepository.cs
+++ src/TvSeriesApi.Data/DAL/Interfaces/IEpisodeRepository.cs
@@ -4,6 +4,7 @@ namespace TvSeriesApi.Data.DAL.Interfaces
 {
     public interface IEpisodeRepository : IBaseRepository<Episode>
     {
+        Task<IEnumerable<Episode>> GetAllEpisodesAsync();
         Task<Episode> GetEpisodeByIdAsync(int id);
         Task<Episode> GetEpisodeWithSeasonAsync(int id);
     }
