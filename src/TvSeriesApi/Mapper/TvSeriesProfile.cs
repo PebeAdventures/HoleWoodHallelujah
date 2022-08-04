b3586045c57b6f74560b53070670407c06bed8b8
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-08-04 13:39:09 +0200

    fix tv series profile

--- src/TvSeriesApi/Mapper/TvSeriesProfile.cs
+++ src/TvSeriesApi/Mapper/TvSeriesProfile.cs
@@ -11,6 +11,31 @@
             CreateMap<TVSeries, TVSeriesCreateDTO>();
             CreateMap<TVSeries, TVSeriesUpdateDTO>();
         }
+
+        //public class ActorProfile : Profile
+        //{
+        //    public ActorProfile()
+        //    {
+        //        CreateMap<Actor, ActorDTO>()
+        //            .ForMember(dest => dest.Episodes,
+        //            opt => opt.MapFrom<ActorIncludesResolver>());
+
+        //        CreateMap<ActorPostDTO, Actor>();
+        //    }
+        //}
+        //public class ActorIncludesResolver : IValueResolver<Actor, ActorDTO, IEnumerable<string>>
+        //{
+        //    public IEnumerable<string> Resolve(Actor source, ActorDTO destination, IEnumerable<string> destMember, ResolutionContext context)
+        //    {
+        //        var episodes = new List<string>();
+        //        if (source.Episodes != null && source.Episodes.Count() > 0)
+        //            foreach (var episode in source.Episodes)
+        //            {
+        //                episodes.Add(episode.Name);
+        //            }
+        //        return episodes.AsEnumerable();
+        //    }
+        //}
     }
 
 }
