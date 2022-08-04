da6d9a1fe096e56af39a4483e9e53119f295f0a1
we3py <CRV374@motorolasolutions.com>
2022-08-04 14:05:18 +0200

    Add Data annotations

--- src/TvSeriesApi/DTO/Episode/EpisodeCreateDTO.cs
+++ src/TvSeriesApi/DTO/Episode/EpisodeCreateDTO.cs
@@ -1,7 +1,14 @@
-﻿namespace TvSeriesApi.DTO.Episode
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TvSeriesApi.DTO.Episode
 {
     public class EpisodeCreateDTO
     {
-        
+        [Required]
+        [DataType("string")]
+        public string Name { get; set; }
+        [Required]
+        [DataType("int")]
+        public int SeasonId { get; set; }
     }
 }
