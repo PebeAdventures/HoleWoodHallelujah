00d2f6a9d1d2bb7010a9d1c5eecfd9045e010852
Artgelika <artgelika.com>
2022-08-04 20:44:25 +0200

    Add mapping to Actor - TvSeries endpoint

--- src/TvSeriesApi/Mapper/ActorProfile.cs
+++ src/TvSeriesApi/Mapper/ActorProfile.cs
@@ -1,14 +1,16 @@
 ﻿namespace TvSeriesApi.Profiles
 {
     public class ActorProfile : Profile
-    {//        public List<string> TVSeriesName { get; set; }
+    {
 
         public ActorProfile()
         {
             CreateMap<Actor, ActorReadDTO>()
                 .ForMember(tv => tv.TVSeriesName, opt => opt.MapFrom(x => x.TVSeries.Select(x => x.Name)));
-            //  CreateMap<Actor, ActorWithTvSeriesDTO>()
-            //      .ForMember(tv => tv.TvSeriesName, act => act.MapFrom(src => src.TVSeries));
+
+            CreateMap<Actor, ActorWithTvSeriesDTO>()
+                .ForMember(tv => tv.TvSeriesName, opt => opt.MapFrom(x => x.TVSeries.Select(x => x.Name)));
+
             CreateMap<ActorCreateDTO, Actor>();
             CreateMap<ActorUpdateDTO, Actor>();
         }
