a603d70981b212f58596fa8ef5ebc2f18803518e
Artgelika <artgelika.com>
2022-08-04 14:03:59 +0200

    Fix the type of list

--- src/TvSeriesApi/DTO/Actor/ActorReadDTO.cs
+++ src/TvSeriesApi/DTO/Actor/ActorReadDTO.cs
@@ -7,6 +7,6 @@ namespace TvSeriesApi.DTO.Actor
         public int ActorId { get; set; }
         public string Fullname { get; set; }
         public int Age { get; set; }
-        public List<TVSeries> TVSeries { get; set; }
+        public List<string> TVSeries { get; set; }
     }
 }
