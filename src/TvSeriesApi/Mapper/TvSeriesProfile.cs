c2fd1ef226b2558157a9aad85ef4d97545e9c172
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-08-04 14:24:05 +0200

    add tv series read dro

--- src/TvSeriesApi/Mapper/TvSeriesProfile.cs
+++ src/TvSeriesApi/Mapper/TvSeriesProfile.cs
@@ -5,9 +5,8 @@
         public TvSeriesProfile()
         {
             CreateMap<TVSeries, TVSeriesReadDTO>()
-                .ForMember(e => e.Genre, opt => opt.MapFrom(e => e.Genre.Name))
-            .ForMember(c => c.Cast, opt => opt.MapFrom(c => c.Cast));
-
+               .ForMember(e => e.Genre, opt => opt.MapFrom(e => e.Genre.Name))
+            .ForMember(c => c.Cast, opt => opt.MapFrom<ActorIncludesResolver>());
             CreateMap<TVSeries, TVSeriesCreateDTO>();
             CreateMap<TVSeries, TVSeriesUpdateDTO>();
         }
@@ -23,19 +22,19 @@
         //        CreateMap<ActorPostDTO, Actor>();
         //    }
         //}
-        //public class ActorIncludesResolver : IValueResolver<Actor, ActorDTO, IEnumerable<string>>
-        //{
-        //    public IEnumerable<string> Resolve(Actor source, ActorDTO destination, IEnumerable<string> destMember, ResolutionContext context)
-        //    {
-        //        var episodes = new List<string>();
-        //        if (source.Episodes != null && source.Episodes.Count() > 0)
-        //            foreach (var episode in source.Episodes)
-        //            {
-        //                episodes.Add(episode.Name);
-        //            }
-        //        return episodes.AsEnumerable();
-        //    }
-        //}
+        public class ActorIncludesResolver : IValueResolver<TVSeries, TVSeriesReadDTO, IEnumerable<string>>
+        {
+            public IEnumerable<string> Resolve(TVSeries source, TVSeriesReadDTO destination, IEnumerable<string> destMember, ResolutionContext context)
+            {
+                var actors = new List<string>();
+                if (source.Cast != null && source.Cast.Count() > 0)
+                    foreach (var actor in source.Cast)
+                    {
+                        actors.Add(actor.Fullname);
+                    }
+                return actors.AsEnumerable();
+            }
+        }
     }
 
 }
