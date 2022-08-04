5265ae7eede15c8efc6170185bba231516111a48
Artgelika <artgelika.com>
2022-08-04 14:03:04 +0200

    Add tests project

---
+++ TvSeriesApi.Tests/ActorServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Moq;
+using TvSeriesApi.Data;
+using TvSeriesApi.Data.Entities;
+using TvSeriesApi.DTO.Actor;
+using TvSeriesApi.Services;
+
+namespace TvSeriesApi.Tests
+{
+    public class ActorServiceTests
+    {
+        //private readonly ActorService _serviceTested; 
+        //private readonly Mock<IUnitOfWork> _actorUnitOfWorkMock = new();
+        //private readonly Mock<IMapper> _mapperMock = new();
+
+        //public ActorServiceTests()
+        //{
+        //    _serviceTested = new ActorService(_actorUnitOfWorkMock.Object, _mapperMock.Object);
+        //}
+
+        //[Test]
+        //public async Task GetActorByIdAsync_ShouldReturnActor_WhenActorExist()
+        //{
+        //    // Arrange
+        //    var actorId = 1;
+        //    var actorName = "Aaron Paul";
+        //    var actorAge = 50;
+        //    List<string> tvSeriesNames = new() { "Breaking Bad" };
+
+        //    var actorReadDto = new ActorReadDTO
+        //    {
+        //        ActorId = actorId,
+        //        Age = actorAge,
+        //        Fullname = actorName,
+        //        TVSeries = tvSeriesNames
+        //    };
+
+        //    var actor = new Actor
+        //    {
+
+        //    };
+        //    _actorUnitOfWorkMock.Setup(x => x.Actors.GetActorByIdAsync(actorId))
+        //        .Returns(actorReadDto);
+        //    // Act
+        //    var actor = await _serviceTested.GetActorByIdAsync(actorId);
+
+        //    // Assert
+        //    Assert.Equals(actor.ActorId, actorId);
+
+        //}
+    }
+}
---
+++ TvSeriesApi.Tests/EpisodesServiceTests.cs
@@ -0,0 +1,58 @@
+using AutoMapper;
+using Moq;
+using TvSeriesApi.Data;
+using TvSeriesApi.Data.Entities;
+using TvSeriesApi.DTO.Episode;
+using TvSeriesApi.Services;
+
+namespace TvSeriesApi.Tests
+{
+    public class EpisodesServiceTests
+    {
+        //private readonly EpisodeService _serviceTested;
+        //private readonly Mock<IUnitOfWork> _episodeUnitOfWorkMock = new();
+        //private readonly Mock<IMapper> _mapperMock = new();
+        
+        //public EpisodesServiceTests()
+        //{
+        //    _serviceTested = new EpisodeService(_episodeUnitOfWorkMock.Object, _mapperMock.Object);
+        //}
+
+        //// linijka 45
+        //[Test]
+        //public async Task GetByIdAsync_ShouldReturnEpisode_WhenEpisodeExists()
+        //{
+        //    // Arrange
+        //    var episodeId = 1;
+        //    var seasonId = 1;
+        //    var episodeName = "White Lines";
+        //    var episodeSeason = "Season 1";
+        //    var episode = new Episode
+        //    {
+        //        EpisodeId = episodeId,
+        //        Name = episodeName,
+        //        SeasonId = seasonId,
+        //        Season = episodeSeason
+        //    };
+
+        //    var episodeReadDto = new EpisodeReadDTO
+        //    {
+        //        Name = episodeName,
+        //        Season = episodeSeason
+        //    };
+
+        //    _episodeUnitOfWorkMock.Setup(x => x.Episodes.GetEpisodeWithSeasonAsync(episodeId))
+        //        .ReturnsAsync();
+
+        //    // Act
+        //    var episode = await _serviceTested.GetEpisodeByIdAsync(episodeId);
+            
+        //    // Assert
+        //    Assert.Equals(episodeId, episode.i)
+            
+        //}
+
+        
+        
+    }
+}
