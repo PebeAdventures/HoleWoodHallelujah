3ba04a01d1789584597067861ba645765ebde1ac
we3py <CRV374@motorolasolutions.com>
2022-08-04 14:04:31 +0200

    Resolve CreateEpisode method

--- src/TvSeriesApi/Services/Interfaces/IEpisodeService.cs
+++ src/TvSeriesApi/Services/Interfaces/IEpisodeService.cs
@@ -3,8 +3,9 @@
     public interface IEpisodeService
     {
         Task<OperationResult<EpisodeReadDTO>> GetEpisodeByIdAsync(int id);
-        Task<OperationResult>DeleteEpisodeAsync(int id);
-        Task<OperationResult> CreateEpisode(EpisodeCreateDTO episode);
+
+        Task<OperationResult> DeleteEpisodeAsync(int id);
+        Task<OperationResult<EpisodeReadDTO>> CreateEpisode(EpisodeCreateDTO episode);
         Task<OperationResult> UpdateEpisodeAsync (int episodeId, EpisodeUpdateDTO episode);
         Task<OperationResult<List<EpisodeReadDTO>>> GetAllEpisodesAsync();
     }
